bfd26eff599592da705de58e71e8e323993c9546
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-30 15:19:34 -0600

    Add files via upload

---
+++ practicando manejos 3/manejos.py
@@ -0,0 +1,10 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+def my_function(fname):
+    print(fname + "Refsnes")
+
+#llamara a la funcion con diferentes argumentos
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
